19b1ee0ed2aacdd5731ad4efd2fca610cc6fd316
Lornop <lorenoelectric@gmail.com>
2025-11-12 22:10:10 -0800

    Got LEDC coded to use PWM. I think I have a better way of averaging my INA226, but im not sure if ive implemented it correctly

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,18 @@
 #include <INA226.h>
 #include <Wire.h>
 
+//*ESP32 LEDC Setup and PWM */
+// use 12 bit precision for LEDC timer
+#define LEDC_TIMER_12_BIT 12
+
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ 5000
+
+// define starting duty, target duty and maximum fade time
+#define LEDC_START_DUTY  (0)
+#define LEDC_TARGET_DUTY (4095)
+
+
 
 // put function declarations here:
 float GetCurrent();         //Get the current reading from the INA226 in mA
@@ -38,7 +50,7 @@ TaskHandle_t Core2;
 
 
 /************************* HARDWARE PINS *************************/
-const int pwm_out     = 04;
+const int PWM_OUT     = 04;
 
 
 /************************* INA226 SETUP  *************************/
@@ -52,19 +64,24 @@ void setup() {
   Serial.begin(115200);
   Wire.begin();
 
-  pinMode(pwm_out, OUTPUT);
-  digitalWrite(pwm_out, LOW);
 
   if (!INA.begin() )
     {
       Serial.println("could not connect. Fix and Reboot");
     }
-  INA.setMaxCurrentShunt(1, 0.002);
+
+  INA.setMaxCurrentShunt(10, 0.002); 
+  INA.setAverage(INA226_4_SAMPLES); 
+
+
 
   // Launch FreeRTOS tasks on both cores
   xTaskCreatePinnedToCore(coreOne, "Core1", 6000, NULL, 1, &Core1, 0);
   xTaskCreatePinnedToCore(coreTwo, "Core2", 6000, NULL, 2, &Core2, 1);
-
+  
+  // Setup the PWM Functionality
+  ledcSetup(0, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
+  ledcAttachPin(PWM_OUT, 0);
 }
 
 
@@ -77,20 +94,12 @@ void loop() {
    CORE 1 TASK (Do PWM)
 *********************************************************************/
 void coreOne(void * pvParameters) {
+  while(1){
 
-  int onTime = timeOn;
-  int offTime = 100 - onTime;
+    int PWMFreq = timeOn;
+    ledcWrite(0, PWMFreq);
 
-  for (int x = 0; x < onTime; x++){                   //Turn the PWM Pin HIGH
-    digitalWrite(pwm_out, HIGH);
-    vTaskDelay(1 / portTICK_PERIOD_MS); // Small delay
   }
-
-  for (int x = 0; x < offTime; x++){                  //Turn the PWM Pin LOW
-    digitalWrite(pwm_out, LOW);
-    vTaskDelay(1 / portTICK_PERIOD_MS); // Small delay
-  }
-
 
 }
 
@@ -99,11 +108,13 @@ void coreOne(void * pvParameters) {
    CORE 2 TASK (READ CURRENT AND ADJUST TIME)
 *********************************************************************/
 void coreTwo(void * pvParameters) {
-  
-  currentNow = GetCurrent();
 
-  timeOn = DoPWMTime(timeOn);
+  while(1){
+    
+    currentNow = GetCurrent();
 
+    timeOn = DoPWMTime(timeOn);
+  }
 
 }
 
@@ -113,11 +124,9 @@ void coreTwo(void * pvParameters) {
 float GetCurrent() {
   float current = 0;
   
-  for (int x =0; x <= 3; x++){
-    current += float(INA.getCurrent_mA());
-    vTaskDelay(5 / portTICK_PERIOD_MS); // Small delay
-  }
-  current = current / 3.0;
+
+  current = INA.getCurrent_mA();
+  
 
   return(current);
 
@@ -128,20 +137,20 @@ float GetCurrent() {
 //Take the current amount of current and either increase or decrease the amount of time to pulse the mosfet pin
 int DoPWMTime(int time) {
 
-  if ((currentNow < max_power) && (time < 100)){      //Crank it up
+  if ((currentNow < max_power) && (time < LEDC_TARGET_DUTY)){      //Crank it up
     time ++;
   }
   
-  if ((currentNow > max_power) && (time > 0)){        //Turn it down
+  if ((currentNow > max_power) && (time > LEDC_START_DUTY)){        //Turn it down
     time --;
   }
 
-  if ((time > 100) || (time < 0)){                    // Something is wrong, just turn it off and start again
+  if ((time > LEDC_TARGET_DUTY) || (time < LEDC_START_DUTY)){       // Something is wrong, just turn it off and start again
     time = 0;   
   }
 
   else {
-    time = time;                                      //Just right
+    time = time;                                                    //Just right
   }
   
   return(time);
