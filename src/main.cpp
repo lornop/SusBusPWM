8958539cfb10e7567245ea3591c2de8f2b6cd146
Lornop <lorenoelectric@gmail.com>
2025-11-12 19:18:15 -0800

    I need to change my digitalwrite to analogwrite and use PWM better than my crappy way

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 
 // put function declarations here:
 float GetCurrent();         //Get the current reading from the INA226 in mA
+int   DoPWMTime(int);       //Figure out how long to have the MOSFET Open
 
 void coreOne(void * pvParameters);
 void coreTwo(void * pvParameters);
@@ -28,8 +29,9 @@ void coreTwo(void * pvParameters);
 
 /************************* GLOBAL VARIABLES *************************/
 
-const float_t max_power = 8;
-float currentNow = 0.0;
+const float_t max_power = 8000;             //Max Power in mA
+float currentNow        = 0.0;              //The INA226 Reading
+int   timeOn            = 0;                //How Long Our PWM pulse is
 
 TaskHandle_t Core1;
 TaskHandle_t Core2;
@@ -62,6 +64,7 @@ void setup() {
   // Launch FreeRTOS tasks on both cores
   xTaskCreatePinnedToCore(coreOne, "Core1", 6000, NULL, 1, &Core1, 0);
   xTaskCreatePinnedToCore(coreTwo, "Core2", 6000, NULL, 2, &Core2, 1);
+
 }
 
 
@@ -75,27 +78,38 @@ void loop() {
 *********************************************************************/
 void coreOne(void * pvParameters) {
 
-int timeOn = 0;
+  int onTime = timeOn;
+  int offTime = 100 - onTime;
 
+  for (int x = 0; x < onTime; x++){                   //Turn the PWM Pin HIGH
+    digitalWrite(pwm_out, HIGH);
+    vTaskDelay(1 / portTICK_PERIOD_MS); // Small delay
+  }
+
+  for (int x = 0; x < offTime; x++){                  //Turn the PWM Pin LOW
+    digitalWrite(pwm_out, LOW);
+    vTaskDelay(1 / portTICK_PERIOD_MS); // Small delay
+  }
 
 
 }
 
 
 /*********************************************************************
-   CORE 2 TASK (READ CURRENT every 100ms)
+   CORE 2 TASK (READ CURRENT AND ADJUST TIME)
 *********************************************************************/
 void coreTwo(void * pvParameters) {
   
-currentNow = GetCurrent();
+  currentNow = GetCurrent();
+
+  timeOn = DoPWMTime(timeOn);
 
 
-  //vTaskDelay(10 / portTICK_PERIOD_MS); // Small delay
 }
 
 /************************* FUNCTIONS *************************/
 
-//Return the current reading from IN226 in mA
+//Return the average of three samples current reading from IN226 in mA
 float GetCurrent() {
   float current = 0;
   
@@ -110,5 +124,28 @@ float GetCurrent() {
 
 }
 
+
+//Take the current amount of current and either increase or decrease the amount of time to pulse the mosfet pin
+int DoPWMTime(int time) {
+
+  if ((currentNow < max_power) && (time < 100)){      //Crank it up
+    time ++;
+  }
+  
+  if ((currentNow > max_power) && (time > 0)){        //Turn it down
+    time --;
+  }
+
+  if ((time > 100) || (time < 0)){                    // Something is wrong, just turn it off and start again
+    time = 0;   
+  }
+
+  else {
+    time = time;                                      //Just right
+  }
+  
+  return(time);
+
+}
 
   
